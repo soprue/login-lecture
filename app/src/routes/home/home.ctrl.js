2588aee5cae91c83540e8e9de52c74d22e767ece
soprue <soprue@naver.com>
2022-05-10 18:36:11 +0900

    프론트 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render('home/index');
-};
+const users = {
+  id: ['test', 'admin', 'root'],
+  password: ['1234', '1234', 'asdf'],
+}
 
-const login = (req, res) => {
-  res.render('home/login');
+const output = {
+  home: (req, res) => {
+    res.render('home/index');
+  },
+  login: (req, res) => {
+    res.render('home/login');
+  },
+}
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+          password = req.body.password;
+
+    if(users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if(users.password[idx] === password) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: '로그인에 실패하였습니다.'
+    });
+  },
 }
 
 module.exports = {
-  home,
-  login
+  output,
+  process
 };
