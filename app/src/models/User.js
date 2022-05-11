94877e2a58f18a308b7c7a49063eae9e3936a0af
soprue <soprue@naver.com>
2022-05-11 15:14:49 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,15 +8,22 @@ class User {
   }
 
   login() {
-    const { id, password } = UserStorage.getUserInfo(this.body.id);
+    const client = this.body;
+    const { id, password } = UserStorage.getUserInfo(client.id);
 
     if(id) {
-      if( id === this.body.id && password === this.body.password ) {
+      if( id === client.id && password === clientclient.password ) {
         return { success: true };
       }
       return { success: false, msg: '비밀번호가 틀렸습니다.' };
     }
     return { success: false, msg: '존재하지 않는 아이디입니다.' };
+  }
+
+  register() {
+    const client = this.body;
+    const response = UserStorage.save(client);
+    return response;
   }
 }
 
---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const id = document.querySelector('#id'),
+      name = document.querySelector('#name'),
+      password = document.querySelector('#password'),
+      confirmPassword = document.querySelector('#confirm-password'),
+      registerBtn = document.querySelector('button');
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+  if(!id.value) return alert("아이디를 입력해 주세요.");
+  if(password.value !== confirmPassword.value) return alert("비밀번호가 일치하지 않습니다.");
+
+  const req = {
+    id: id.value,
+    name: name.value,
+    password: password.value
+  };
+
+  fetch('/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    if(res.success) {
+      location.href = '/';
+    } else {
+      alert(res.msg);
+    }
+  })
+  .catch((err) => {
+    console.error(new Error('회원가입 중 에러 발생'));
+  });
+}
