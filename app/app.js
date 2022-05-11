4295867b9c507ee16998560029e7ca987a76bfde
soprue <soprue@naver.com>
2022-05-11 18:58:24 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 "use strict";
 
+// 모듈
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 
 // 앱 세팅
@@ -11,6 +15,7 @@ app.use(express.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({ extended : true }));
 
+// 라우팅
 const home = require('./src/routes/home');
 app.use('/', home); // use -> 미들웨어를 등록해 주는 메서드
 
